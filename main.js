14c5214a7587555147d7f96b7a8339a20e8c81c4
Filip Procházka <prochazka@unodor.cz>
2023-03-09 18:56:07 +0100

    update

---
+++ main.js
@@ -0,0 +1,62 @@
+
+const goodbye = (name) => {
+	//return 'Ahoj ' + name
+	return `Ahoj ${name}`
+}
+
+/*
+Hejsci na kolech : Šance, že zásilka bude skutečně doručena, je 90%. Pokud se doručení povede, je to vždy do 30 minut do okolí 5 km.
+
+Machři, co maj drony : Šance, že zásilka bude skutečně doručena, je 50%. Pokud se doručení povede, je to vždy do deseti minut po celé Praze.
+
+Borci v autech : Zásilka je doručena vždy. Doručení však může trvat až dvě hodiny.
+
+Napišme si funkce, které simulují naše doručovací společnosti.
+*/
+
+
+const hejsci = (zasilka) => {
+	if(Math.random() > 0.1) {
+		return `Zásilka: „${zasilka}“ bude doručena za 30 minut, kámo.` 
+	}
+	return `Zásilka: „${zasilka}“ se ztratila. Kurýra přepadla smečka hladových bezdomovců.`
+}
+
+const machri = (zasilka) => {
+	if(Math.random() > 0.5) {
+		return `Zásilka: „${zasilka}“ bude doručena za 10 minut.` 
+	}
+	return `Zásilka: „${zasilka}“ se ztratila. Drona přepadlo hejno hladových holubů.`
+}
+
+const borci = (zasilka) => {
+	return `Zásilka: „${zasilka}“ bude doručena s přehledem za 120 minut.` 
+}
+
+const objednat = (jidlo, doruceni) => {
+	const cislo = Math.floor(Math.random() * 1000)
+	const id = String(cislo).padStart(4, '0')
+	const balicek = `${jidlo} (${id})`
+
+	return doruceni(balicek)
+}
+
+const calc = (num1, op, num2) => {
+	return op(num1,num2)
+}
+
+const plus = (num1, num2) => {
+	return num1 + num2
+}
+
+const minus = (num1, num2) => {
+	return num1 - num2
+}
+
+const times = (num1, num2) => {
+	return num1 * num2
+}
+
+const divide = (num1, num2) => {
+	return num1 / num2
+}
