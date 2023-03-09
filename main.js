34d0ee7d6fe38d27141c9a99b935a54046810ce8
Filip Procházka <prochazka@unodor.cz>
2023-03-09 20:34:52 +0100

    komplet ukazky z lekce

--- main.js
+++ main.js
@@ -1,62 +1,79 @@
+// Zpoždění a časovače
 
-const goodbye = (name) => {
-	//return 'Ahoj ' + name
-	return `Ahoj ${name}`
+
+// Zpoždění
+const timesIsUp = () => {
+    document.body.innerHTML += '<p>Čas vypršel</p>'
 }
+
+let cislo = 0
+const pricitej = () => {
+    cislo += 1
+    document.body.innerHTML += cislo
+}
+
+setTimeout(timesIsUp, 5000)
+
+// Volání v intervalu
+
+setInterval(pricitej, 10000)
 
 /*
-Hejsci na kolech : Šance, že zásilka bude skutečně doručena, je 90%. Pokud se doručení povede, je to vždy do 30 minut do okolí 5 km.
+const mojeSuperFunkce = ()=>{
+    console.log("Uběhlo 5 vteřin a zavolala se anonymní funkce")
+}*/
 
-Machři, co maj drony : Šance, že zásilka bude skutečně doručena, je 50%. Pokud se doručení povede, je to vždy do deseti minut po celé Praze.
+// setTimeout(mojeSuperFunkce, 5000)
 
-Borci v autech : Zásilka je doručena vždy. Doručení však může trvat až dvě hodiny.
+setTimeout(
+    ()=>{
+        console.log("Uběhlo 5 vteřin a zavolala se anonymní funkce")
+    }
+    /*function(){
+        console.log("Uběhlo 5 vteřin a zavolala se anonymní funkce")
+    }*/
+    ,50000)
 
-Napišme si funkce, které simulují naše doručovací společnosti.
+// druhy událostí: https://www.w3schools.com/jsref/dom_obj_event.asp 
+
+const changeTitle = () => {
+    const h1Elm = document.querySelector('h1')
+    h1Elm.textContent = 'Baf!'
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', changeTitle)
+
+document.addEventListener('scroll', changeTitle)
+
+/*
+window.addEventListener('contextmenu', (ev) => {
+    alert('right clicked')
+});*/
+
+
+// Cvičení
+// https://kodim.cz/kurzy/javascript-vyuka/js-1/hof-udalosti/cv-udalosti
+
+/*
+Objednávka
+
+Vytvoříme jednoduchou stránku s objednávacím tlačítkem.
+
+Založte HTML stránku s jedním tlačítkem
+a) <button id="btn-order">Objednat</button>
+
+b) Doplňte do stránky JavaScriptový program, který zařídí, že po stisknutí tlačítka se do stránky za tlačítko vypíše odstavec:
+<p>Objednáno</p>
+
+c) Upravte program tak, že text se nevypíše do stránky, ale zobrazí se na samotném tlačítku.
 */
 
+const orderBtn = document.querySelector('#btn-order')
 
-const hejsci = (zasilka) => {
-	if(Math.random() > 0.1) {
-		return `Zásilka: „${zasilka}“ bude doručena za 30 minut, kámo.` 
-	}
-	return `Zásilka: „${zasilka}“ se ztratila. Kurýra přepadla smečka hladových bezdomovců.`
+const afterClickOrder = () => {
+    //document.body.innerHTML += '<p> Objednáno </p>'
+    orderBtn.textContent = 'Objednáno'
 }
 
-const machri = (zasilka) => {
-	if(Math.random() > 0.5) {
-		return `Zásilka: „${zasilka}“ bude doručena za 10 minut.` 
-	}
-	return `Zásilka: „${zasilka}“ se ztratila. Drona přepadlo hejno hladových holubů.`
-}
-
-const borci = (zasilka) => {
-	return `Zásilka: „${zasilka}“ bude doručena s přehledem za 120 minut.` 
-}
-
-const objednat = (jidlo, doruceni) => {
-	const cislo = Math.floor(Math.random() * 1000)
-	const id = String(cislo).padStart(4, '0')
-	const balicek = `${jidlo} (${id})`
-
-	return doruceni(balicek)
-}
-
-const calc = (num1, op, num2) => {
-	return op(num1,num2)
-}
-
-const plus = (num1, num2) => {
-	return num1 + num2
-}
-
-const minus = (num1, num2) => {
-	return num1 - num2
-}
-
-const times = (num1, num2) => {
-	return num1 * num2
-}
-
-const divide = (num1, num2) => {
-	return num1 / num2
-}
+orderBtn.addEventListener('click', afterClickOrder)
